8ae32824e5f41ff3652056d6dcbd0c3f4c9046f0
Darren <you@example.comdarren@kandekore.net>
2022-07-19 14:54:25 +0100

    special charachters

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-// Assignment code here
-
+// Generate Special characters for the function
+const specialCharacters = "!@#$%^&*()";
+const generateButton = document.getElementById("generateBtn");
+generateButton.addEventListener("click", writePassword); // Assignment code here
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -10,7 +12,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
