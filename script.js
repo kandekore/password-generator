4c7390eb792e4d773b250730e9c40feb788a8505
kandekore <41653646+kandekore@users.noreply.github.com>
2022-07-21 23:48:03 +0100

    Final Edits

--- script.js
+++ script.js
@@ -1,96 +1,12 @@
-const specialArr = [
-  "!",
-  "@",
-  "#",
-  "$",
-  "%",
-  "^",
-  "&",
-  "*",
-  "(",
-  ")",
-  "+",
-  "-",
-  ".",
-  "~",
-  "|",
-  "<",
-  ">",
-  "=",
-  "-",
-  "_",
-  "/",
-  ":",
-  ";",
-  "?",
-  "[",
-  "]",
-  "{",
-  "}",
-  "~",
-];
-const lowerCaseArr = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-];
-const upperCaseArr = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
-];
+// Arrays
+const specialArr = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "-", ".", "`", "~", "|", "<", ">", "=", "-", "_"];
+const lowerCaseArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const upperCaseArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 const numbersArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+// Get references to the #generateBtn element
 const generateButton = document.getElementById("generateBtn");
 generateButton.addEventListener("click", writePassword);
-
-// Get references to the #generate element
-//var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
@@ -200,5 +116,4 @@ function generatePassword() {
 
   return randomPasswordGenerated;
 }
-// Add event listener to generate button
-//generateBtn.addEventListener("click", writePassword);
+
