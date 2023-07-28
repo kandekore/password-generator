fd1692938580daba57752683ef220b3f01d24d99
kandekore <darren@kandekore.net>
2023-07-28 14:22:11 +0100

    copy to clipboard

--- script.js
+++ script.js
@@ -4,9 +4,12 @@ const lowerCaseArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l"
 const upperCaseArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 const numbersArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-// Get references to the #generateBtn element
+// Get references to the #generateBtn and #copyBtn elements
 const generateButton = document.getElementById("generateBtn");
+const copyButton = document.getElementById("copyBtn");
+
 generateButton.addEventListener("click", writePassword);
+copyButton.addEventListener("click", copyToClipboard);
 
 // Write password to the #password input
 function writePassword() {
@@ -14,6 +17,15 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+  copyButton.disabled = false; // Enable the "Copy to Clipboard" button after generating the password
+}
+
+// Function to copy password to clipboard
+function copyToClipboard() {
+  var passwordText = document.querySelector("#password");
+  passwordText.select();
+  document.execCommand("copy");
+  alert("Password copied to clipboard!");
 }
 
 // Prompts and Validation
